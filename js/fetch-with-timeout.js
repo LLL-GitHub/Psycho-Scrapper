0500987d681ffccd52e08752804757ba240710c0
LLL-GitHub <36159244+LLL-GitHub@users.noreply.github.com>
2018-11-14 10:54:01 -0600

    Delete fetch-with-timeout.js

--- js/fetch-with-timeout.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-function Fetch(input, init, timeout) {
-  return Promise.race([
-    fetch(input, init),
-    new Promise((resolve, reject) =>
-      setTimeout(() => reject(new Error(`Fetch timeout.`)), timeout)
-    )
-  ]);
-}
